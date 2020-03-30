a3b3eab6ef21127d0787b91785f4820afa22445b
Danial Goodwin <danialgoodwin@gmail.com>
2020-03-30 00:00:46 -0600

    Add to AST

--- src/anon_ast.py
+++ src/anon_ast.py
@@ -34,8 +34,7 @@ class VarArithmeticExpression(ArithmeticExpression):
     def eval(self, env):
         if self.name in env:
             return env[self.name]
-        else:
-            return 0
+        return 0
 
     def as_html(self, env):
         return self.name
@@ -49,6 +48,24 @@ class BinaryOpArithmeticExpression(ArithmeticExpression):
 
     def __repr__(self):
         return f'BinaryOpAexp({self.op}, {self.left}, {self.right})'
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        if self.op == '+':
+            value = left_value + right_value
+        elif self.op == '-':
+            value = left_value - right_value
+        elif self.op == '*':
+            value = left_value * right_value
+        elif self.op == '/':
+            value = left_value / right_value
+        else:
+            raise RuntimeError('unknown operator: ' + self.op)
+        return value
+
+    def as_html(self, env):
+        return f'{self.left.as_html()} {self.op.as_html()} {self.right.as_html()}'
 
 
 ###################################################################################################
@@ -66,23 +83,72 @@ class BooleanExpression(Equality):
 
 class RelationalOpBooleanExpression(BooleanExpression):
     def __init__(self, op, left: ArithmeticExpression, right: ArithmeticExpression):
-        ...
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        if self.op == '<':
+            value = left_value < right_value
+        elif self.op == '<=':
+            value = left_value <= right_value
+        elif self.op == '>':
+            value = left_value > right_value
+        elif self.op == '>=':
+            value = left_value >= right_value
+        elif self.op == '=':
+            value = left_value == right_value
+        elif self.op == '!=':
+            value = left_value != right_value
+        else:
+            raise RuntimeError('unknown operator: ' + self.op)
+        return value
+
+    def as_html(self):
+        return f'{self.left.as_html()} {self.op.as_html()} {self.right.as_html()}'
 
 
 class AndBooleanExpression(BooleanExpression):
     def __init__(self, left: BooleanExpression, right: BooleanExpression):
-        ...
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        return left_value and right_value
+
+    def as_html(self):
+        return f'{self.left.as_html()} {self.op.as_html()} {self.right.as_html()}'
+
 
 
 class OrBooleanExpression(BooleanExpression):
     def __init__(self, left: BooleanExpression, right: BooleanExpression):
-        ...
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        return left_value or right_value
+
+    def as_html(self):
+        return f'{self.left.as_html()} {self.op.as_html()} {self.right.as_html()}'
 
 
 class NotBooleanExpression(BooleanExpression):
     def __init__(self, exp: BooleanExpression):
-        ...
+        self.exp = exp
 
+    def eval(self, env):
+        value = self.exp.eval(env)
+        return not value
+
+    def as_html(self):
+        return 'not'
 
 ###################################################################################################
 
@@ -94,19 +160,61 @@ class Statement(Equality):
 
 class AssignStatement(Statement):
     def __init__(self, name, aexp):
-        ...
+        self.name = name
+        self.aexp = aexp
+
+    def eval(self, env):
+        value = self.aexp.eval(env)
+        env[self.name] = value
+
+    def as_html(self):
+        return f'{self.name.as_html()} = {self.aexp.as_html()}'
 
 
 class CompoundStatement(Statement):
     def __init__(self, first, second):
-        ...
+        self.first = first
+        self.second = second
+
+    def eval(self, env):
+        self.first.eval(env)
+        self.second.eval(env)
+
+    def as_html(self):
+        return f'{self.first.as_html()} {self.second.as_html()}'
 
 
 class IfStatement(Statement):
     def __init__(self, condition, true_stmt, false_stmt):
-        ...
+        self.condition = condition
+        self.true_stmt = true_stmt
+        self.false_stmt = false_stmt
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        if condition_value:
+            self.true_stmt.eval(env)
+        else:
+            if self.false_stmt:
+                self.false_stmt.eval(env)
+
+    def as_html(self):
+        output = f'if {self.condition.as_html()} {self.true_stmt.as_html()}'
+        if self.false_stmt:
+            output += f'else: {self.false_stmt.as_html()}'
+        return
 
 
 class LoopStatement(Statement):
     def __init__(self, condition, body):
-        ...
+        self.condition = condition
+        self.body = body
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        while condition_value:
+            self.body.eval(env)
+            condition_value = self.condition.eval(env)
+
+    def as_html(self):
+        return f'while ({self.condition.as_html()}) {self.body.as_html()}'
